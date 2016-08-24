3b98df3418875321b1df721664892cd9942469ca
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-24 17:16:05 +0200

    Test

---
+++ test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import os, sys, platform, subprocess, tempfile, shutil;
+
+out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
+print("out: '" + out.decode().rstrip() + "'");
