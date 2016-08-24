c8b36f6d4707b15b51d1a8d38cd9594c5bbbc6a7
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-24 18:08:01 +0200

    Test

--- test.py
+++ test.py
@@ -4,8 +4,5 @@ import os, sys, platform, subprocess, tempfile, shutil;
 out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
 print("out: '" + out.decode().rstrip() + "'" + os.linesep);
 
-out = subprocess.check_output(["adb", "version"], stderr=subprocess.STDOUT);
-print("out: '" + out.decode().rstrip() + "'");
-
 out = subprocess.check_output(["zipalign"], stderr=subprocess.STDOUT);
 print("out: '" + out.decode().rstrip() + "'");
