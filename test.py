9956329c0f56299052d5623e27313a6bee92dd71
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-25 20:04:39 +0200

    Test

--- test.py
+++ test.py
@@ -3,6 +3,3 @@ import os, sys, platform, subprocess, tempfile, shutil;
 
 out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
 print("out: '" + out.decode().rstrip() + "'" + os.linesep);
-
-out = subprocess.check_output(["zipalign"], stderr=subprocess.STDOUT);
-print("out: '" + out.decode().rstrip() + "'");
