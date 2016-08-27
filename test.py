35e5a7539877aa8103d9833154e858cda59b07e6
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-27 19:12:31 +0200

    Test

--- test.py
+++ test.py
@@ -1,8 +1,53 @@
 #!/usr/bin/env python
-import os, sys, platform, subprocess, tempfile, shutil;
+import subprocess;
 
-out = subprocess.check_output(["python", "-V"], stderr=subprocess.STDOUT);
-print("out: '" + out.decode().rstrip() + "'" + os.linesep);
+def initialize():
+    class ExtendedCalledProcessError(subprocess.CalledProcessError):
+        def __init__(self, returncode, cmd, output=None, stderr=None):
+            super(self.__class__, self).__init__(returncode, cmd, output, stderr);
+            self.returncode += 1;
+            #self.returncode = returncode;
+            #self.cmd = cmd;
+            #self.stdout = self.output = output;
+            #self.stderr = stderr;
+    subprocess.ExtendedCalledProcessError = ExtendedCalledProcessError;
 
-out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
-print("out: '" + out.decode().rstrip() + "'" + os.linesep);
+#if "check_output" not in dir(subprocess):
+    def check_output(*popenargs, **kwargs):
+        if "stdout" in kwargs:
+            raise ValueError("stdout argument not allowed, it will be overridden.");
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs);
+        stdout_data, __ = process.communicate();
+        ret_code = process.poll();
+        if ret_code is None:
+            raise RuntimeWarning("The process is not yet terminated.");
+        if ret_code:
+            cmd = kwargs.get("args");
+            if cmd is None:
+                cmd = popenargs[0];
+            raise subprocess.CalledProcessError(returncode=ret_code, cmd=cmd, output=stdout_data, stderr=None);
+        return stdout_data;
+    subprocess.check_output2 = check_output;
+
+
+
+import os;
+initialize();
+def test(one = True):
+    if one: return subprocess.check_output(["java", "-v"], stderr=subprocess.STDOUT);
+    return subprocess.check_output2(["java", "-v"], stderr=subprocess.STDOUT);
+
+
+try:
+    test(False);
+except subprocess.CalledProcessError as e:
+    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
+    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+str(e.stdout)+os.linesep+str(e.stderr));#e.output.decode("utf-8").strip()
+
+print(os.linesep+os.linesep+os.linesep);
+
+try:
+    test(True);
+except subprocess.CalledProcessError as e:
+    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
+    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+str(e.stdout)+os.linesep+str(e.stderr));
