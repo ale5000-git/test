77619901f437c44935db3abe3618652cb0a8b574
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-27 13:59:40 +0200

    Test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import os, sys, platform, subprocess, tempfile, shutil;
 
+out = subprocess.check_output(["python", "-V"], stderr=subprocess.STDOUT);
+print("out: '" + out.decode().rstrip() + "'" + os.linesep);
+
 out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
 print("out: '" + out.decode().rstrip() + "'" + os.linesep);
