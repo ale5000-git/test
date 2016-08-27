e3de14fa23a625d8e0133d9df36b210caa99b323
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-28 00:43:15 +0200

    Test

--- test.py
+++ test.py
@@ -1,45 +1,9 @@
 #!/usr/bin/env python
-
-def initialize():
-    import subprocess;
-
-    class ExtendedCalledProcessError(subprocess.CalledProcessError):
-        def __init__(self, returncode, cmd, output=None, stderr=None):
-            try:
-                super(self.__class__, self).__init__(returncode=returncode, cmd=cmd, output=output);
-            except TypeError:
-                super(self.__class__, self).__init__(returncode=returncode, cmd=cmd);
-                self.output = output;
-            if getattr(self, "stdout", False) == False: self.stdout = output;
-            self.returncode += 1;###########
-            self.stderr = stderr;
-
-    def extended_check_output(*popenargs, **kwargs):
-        if "stdout" in kwargs:
-            raise ValueError("stdout argument not allowed, it will be overridden.");
-        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs);
-        stdout_data, __ = process.communicate();
-        ret_code = process.poll();
-        if ret_code is None:
-            raise RuntimeWarning("The process is not yet terminated.");
-        if ret_code:
-            cmd = kwargs.get("args");
-            if cmd is None:
-                cmd = popenargs[0];
-            raise ExtendedCalledProcessError(returncode=ret_code, cmd=cmd, output=stdout_data, stderr=None);
-        return stdout_data;
-
-    try:
-        from subprocess import check_output;
-    except ImportError:
-        print("111111111111111111111111111");
-        subprocess.check_output = extended_check_output;
-
-
-
-initialize();
 import os;
 import subprocess;
+import libs.compat as CompatLayer;
+CompatLayer.subprocess();
+
 try:
     subprocess.check_output(["java", "-v"], stderr=subprocess.STDOUT);
 except subprocess.CalledProcessError as e:
