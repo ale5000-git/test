476efe7b00c294bf87f39962f3127d3daf957e55
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-27 20:46:51 +0200

    Test

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-import subprocess;
 
 def initialize():
+    import subprocess;
+
     class ExtendedCalledProcessError(subprocess.CalledProcessError):
         def __init__(self, returncode, cmd, output=None, stderr=None):
             try:
-                super(self.__class__, self).__init__(returncode, cmd, output);
+                super(self.__class__, self).__init__(returncode=returncode, cmd=cmd, output=output);
             except TypeError:
-                super(self.__class__, self).__init__(returncode, cmd);
+                super(self.__class__, self).__init__(returncode=returncode, cmd=cmd);
                 self.output = output;
             if getattr(self, "stdout", False) == False: self.stdout = output;
             self.returncode += 1;###########
             self.stderr = stderr;
 
-#if "check_output" not in dir(subprocess):
-    def check_output(*popenargs, **kwargs):
+    def extended_check_output(*popenargs, **kwargs):
         if "stdout" in kwargs:
             raise ValueError("stdout argument not allowed, it will be overridden.");
         process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs);
@@ -28,27 +28,20 @@ def initialize():
                 cmd = popenargs[0];
             raise ExtendedCalledProcessError(returncode=ret_code, cmd=cmd, output=stdout_data, stderr=None);
         return stdout_data;
-    subprocess.check_output2 = check_output;
+
+    try:
+        from subprocess import check_output;
+    except ImportError:
+        print("111111111111111111111111111");
+        subprocess.check_output = extended_check_output;
 
 
 
-import os;
 initialize();
-def test(one = True):
-    if one: return subprocess.check_output(["java", "-v"], stderr=subprocess.STDOUT);
-    return subprocess.check_output2(["java", "-v"], stderr=subprocess.STDOUT);
-
-
+import os;
+import subprocess;
 try:
-    test(False);
+    subprocess.check_output(["java", "-v"], stderr=subprocess.STDOUT);
 except subprocess.CalledProcessError as e:
     print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out1: "+str(e.output).strip()+os.linesep+"Out2: "+str(e.stdout).strip()+os.linesep+"Err: "+str(e.stderr).strip());
-
-print(os.linesep+os.linesep+os.linesep);
-
-try:
-    pass#test(True);
-except subprocess.CalledProcessError as e:
-    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+str(e.output).strip()+os.linesep+str(""));#e.output.decode("utf-8").strip()
+    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
