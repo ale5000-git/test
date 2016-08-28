7731f3a5ae1de81b1195a40048c1f00fe91bf4e3
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-28 11:11:13 +0200

    Test

--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 import os;
 import subprocess;
 import libs.compat as CompatLayer;
-CompatLayer.subprocess();
+CompatLayer.fix_all();
+
+dir_list = tuple(sorted(os.listdir(".")));
+print(dir_list);
 
 try:
     subprocess.check_output(["java", "-v"], stderr=subprocess.STDOUT);
 except subprocess.CalledProcessError as e:
     print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
     print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
-
-dir_list = tuple(sorted(os.listdir(".")));
-print(dir_list);
