335df4297e9e101b2ddd93cfcfd9ab59c2cfb30a
ale5000 <ale5000-git@users.noreply.github.com>
2016-08-28 22:11:32 +0200

    Test

--- test.py
+++ test.py
@@ -9,19 +9,23 @@ CompatLayer.fix_all();
 #print(dir_list);
 
 try:
-    subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
+    out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
+    print(os.linesep+os.linesep+"Good: "+out);
 except subprocess.CalledProcessError as e:
     print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
     print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
 
 try:
-    subprocess.check_output(["7za", "-h"], stderr=subprocess.STDOUT);
+    
+    out = subprocess.check_output(["7za", "-h"], stderr=subprocess.STDOUT);
+    print(os.linesep+os.linesep+"Good: "+out);
 except subprocess.CalledProcessError as e:
     print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
     print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
 
 try:
-    subprocess.check_output(["7za", "-v"], stderr=subprocess.STDOUT);
+    out = subprocess.check_output(["7za", "-v"], stderr=subprocess.STDOUT);
+    print(os.linesep+os.linesep+"Good: "+out);
 except subprocess.CalledProcessError as e:
     print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
     print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
