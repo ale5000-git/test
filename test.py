49513006cfe765c0c013c74c359029b3f6bc92cd
ale5000 <ale5000-git@users.noreply.github.com>
2016-09-02 11:24:47 +0200

    Test

--- test.py
+++ test.py
@@ -10,29 +10,29 @@ import compatlayer;
 compatlayer.fix_all();
 
 #dir_list = tuple(sorted(os.listdir(".")));
-#print(dir_list);
+#print_(dir_list);
 
 try:
     out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
-    print(os.linesep+os.linesep+"Good: "+out.decode());
+    print_(os.linesep+os.linesep+"Good: "+out.decode());
 except subprocess.CalledProcessError as e:
-    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
+    print_(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
+    print_("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
 
 try:
     
     out = subprocess.check_output(["7za", "-h"], stderr=subprocess.STDOUT);
-    print(os.linesep+os.linesep+"Good: "+out.decode());
+    print_(os.linesep+os.linesep+"Good: "+out.decode());
 except subprocess.CalledProcessError as e:
-    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
+    print_(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
+    print_("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
 
 try:
     out = subprocess.check_output(["7za", "-v"], stderr=subprocess.STDOUT);
-    print(os.linesep+os.linesep+"Good: "+out.decode());
+    print_(os.linesep+os.linesep+"Good: "+out.decode());
 except subprocess.CalledProcessError as e:
-    print(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
+    print_(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
+    print_("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
 
 print_("abc", "def");
 print_("1, 2, 3...", end="");
