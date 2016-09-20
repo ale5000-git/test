1e18bf019f89a22d87c9f1c6b09edb649d6c06d9
ale5000 <ale5000-git@users.noreply.github.com>
2016-09-20 02:00:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,9 @@ import compatlayer;
 compatlayer.fix_all();
 
 try:
-    out = subprocess.check_output(["java", "-version"], stderr=subprocess.STDOUT);
+    out = subprocess.check_output(["java", "-versionX"], stderr=subprocess.STDOUT);
     print_(os.linesep+os.linesep+"Output: "+out.decode());
 except subprocess.CalledProcessError as e:
     print_(os.linesep+os.linesep+"E: "+str(e)+os.linesep);
-    print_("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep+"Out: "+e.output.decode("utf-8").strip());
+    print_("Return: "+str(e.returncode)+os.linesep+"Cmd: "+str(e.cmd)+os.linesep);
+    print_("Out: "+e.output.decode("utf-8").strip()+os.linesep+"Out: "+e.stdout.decode("utf-8").strip()+os.linesep+"Err: "+str(e.stderr));
