fab07350fad94b8303a841a460cf522f4cd452ac
ale5000 <ale5000-git@users.noreply.github.com>
2016-11-15 20:33:25 +0100

    Update and rename conf.py.bak to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-]
+##extensions = [
+##    'sphinx.ext.autodoc',
+##]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -15,24 +15,24 @@ extensions = [
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+##master_doc = 'index'
 
 # General information about the project.
-project = 'Test'
-copyright = '2016, ale5000'
-author = 'ale5000'
+##project = 'Test'
+##copyright = '2016, ale5000'
+##author = 'ale5000'
 
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+##language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+##exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+##pygments_style = 'sphinx'
 
 
 # -- Options for HTML output ----------------------------------------------
@@ -40,12 +40,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+##html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+##html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PyCompatLayerdoc'
+htmlhelp_basename = 'TestModuleName'
