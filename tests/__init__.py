9de58d963689554500e1e8c4bdaa545641245caf
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-06 12:41:52 +0200

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def custom_test_suite():
+    try:
+        if sys.version_info <= (3, 4):
+            import unittest2 as unittest
+        else:
+            import unittest
+    except ImportError:
+        import unittest
+    return unittest.TestLoader().discover("tests", pattern="*_test.py")
