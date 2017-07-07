18cc7ec374128f5007f45c6e7e30fc6f22f003d5
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-07 16:46:57 +0200

    Update pycompatlayer_test.py

--- tests/pycompatlayer_test.py
+++ tests/pycompatlayer_test.py
@@ -6,8 +6,8 @@ class PyCompatLayerTestCase(unittest.TestCase):
     """Tests for `pycompatlayer.py`."""
 
     def test_fix_all(self):
-        print(sys.maxint)
         import sys
+        print(sys.maxint)
         if 'maxsize' not in sys.__dict__:
             print('dohhhhhhhhhhhhh')
         self.assertTrue(pycompatlayer.fix_all())
