16b66cb1ce958147efaddad829b1c9df99494785
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-07 14:25:56 +0200

    Update run_codecov.sh

--- .travis/run_codecov.sh
+++ .travis/run_codecov.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo Coverage testing...
+echo '*** Coverage testing...'
 
-if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  coverage run "$1" || exit 1
-else
-  coverage run setup.py test || exit 1
-fi
+coverage run setup.py test || exit 1
 
-echo Done.
+#if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+  #coverage run "$1" || exit 1
+#else
+  #coverage run setup.py test || exit 1
+#fi
+
+echo '*** Done.'
