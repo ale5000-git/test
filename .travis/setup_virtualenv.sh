367ee3ca5a28a8acdcf3a20a3c6a9762ff491617
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-07 09:52:24 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -6,11 +6,15 @@ virtualenv -p "/usr/bin/python$1" "~/virtualenv/python$1"
 source "~/virtualenv/python$1/bin/activate"
 export TRAVIS_PYTHON_VERSION="$1"
 
-easy_install --version
+#easy_install --version
 
-if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  pip install distribute==0.6.49 || exit 1
-fi
+#if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+  #pip install distribute==0.6.49 || exit 1
+#fi
+
+
+
+
 # ==1.4.2
 
 ######### python-setuptools (but you can install python-setuptools-deadsnakes)
