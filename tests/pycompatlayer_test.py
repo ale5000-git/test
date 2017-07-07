4c7722f78c2ef9ae8e5a2b039b58ec822b4c6ab6
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-07 18:48:05 +0200

    Update pycompatlayer_test.py

--- tests/pycompatlayer_test.py
+++ tests/pycompatlayer_test.py
@@ -6,10 +6,6 @@ class PyCompatLayerTestCase(unittest.TestCase):
     """Tests for `pycompatlayer.py`."""
 
     def test_fix_all(self):
-        import sys
-        print(sys.maxint)
-        if 'maxsize' not in sys.__dict__:
-            print('dohhhhhhhhhhhhh')
         self.assertTrue(pycompatlayer.fix_all())
 
         # The code is executed twice during the test in some cases,
@@ -20,3 +16,6 @@ class PyCompatLayerTestCase(unittest.TestCase):
         except ImportError:
             import __builtin__ as builtins
         del builtins.pycompatlayer
+
+if __name__ == "__main__":
+    unittest.main()
