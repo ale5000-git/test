e6971f9e86cfe1f98fb3c34f014168952097740c
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 00:07:56 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -13,6 +13,7 @@ if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
   easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
   easy_install --version
   easy_install distribute==0.7.3 || exit 1
+  easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
   easy_install --version
 
   #echo '*** Installing Python Pip...'
@@ -25,6 +26,7 @@ elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
   easy_install --version
   easy_install distribute==0.7.3 || exit 1
+  easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
   easy_install --version
 
   echo '*** Installing Python Pip...'
