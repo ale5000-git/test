b6f019e3d9977d8a992d8be14752275665ea5cd6
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 00:29:23 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -8,32 +8,19 @@ source "~/virtualenv/python$1/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$1"
 
-if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
-  echo '*** Installing Python Setuptools...'
+if [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+  echo '*** Installing Python Setuptools (part 1)...'
   easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' || exit 1
   rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
-  easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
-  easy_install --version
 
-  #echo '*** Installing Python Pip...'
-  #easy_install pip==1.3.1 || exit 1
+  echo '*** Installing Python Pip...'
+  if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then pip install pip==1.3.1 || exit 1; else pip install pip==1.1 || exit 1; fi
 
-
-elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  echo '*** Installing Python Setuptools...'
-  easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' || exit 1
-  rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
-  easy_install 'http://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
-  easy_install --version
-
-  #echo '*** Installing Python Pip...'
-  #pip install pip==1.1 || exit 1
-
-  echo '*** Installing Python Setuptools...'
   #pip install setuptools==0.7.3 > /dev/null 2>&1 || exit 1
-  #rm -r "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
-  #easy_install --version
-  #pip install setuptools==1.4.2 || exit 1
+
+  echo '*** Installing Python Setuptools (part 2)...'
+  pip install setuptools==1.4.2 || exit 1
+  easy_install --version
 fi
 
 echo '*** Done.'
