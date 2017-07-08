f5795081bdfe2ab07c632af5f7cff235c17a232d
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 01:07:37 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '*** Python Virtualenv setup in progress...'
+echo '*** Python - Virtualenv setup in progress...'
 virtualenv "~/virtualenv/python$1" || exit 1
 rm -f "~/virtualenv/python$1/bin/python" || exit 1
 virtualenv -p "/usr/bin/python$1" "~/virtualenv/python$1" || exit 1
@@ -9,16 +9,16 @@ source "~/virtualenv/python$1/bin/activate" || exit 1
 export TRAVIS_PYTHON_VERSION="$1"
 
 if [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  echo '*** Installing Python Setuptools...'
+  echo '*** Python - Installing Setuptools...'
   easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
   rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
   easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
 
-  echo '*** Installing Python Pip...'
-  #if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then pip install pip==1.3.1 || exit 1; else pip install pip==1.1 || exit 1; fi
+  echo '*** Python - Installing Pip...'
+  if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then pip install pip==1.3.1 || exit 1; else pip install pip==1.1 || exit 1; fi
 
-  echo '-------------------------------'
-  pip install ssl==1.14
+  echo '*** Python - Installing SSL...'
+  pip install ssl
 fi
 
 echo '*** Done.'
