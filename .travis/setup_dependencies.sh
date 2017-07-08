cde61a12570a465f09dfdfa6e1ef911e87975ef4
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-08 23:50:48 +0200

    Update setup_dependencies.sh

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -5,7 +5,7 @@ echo '*** Installing dependencies...'
 if [[ $TRAVIS_PYTHON_VERSION == '3.2' ]]; then
   pip install coverage==4.0a5 || exit 1
   pip install requests==2.10.0 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+elif [[ $TRAVIS_PYTHON_VERSION == '2.4' || $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
   pip install coverage==3.7.1 || exit 1
   #pip install requests==2.7.9 || exit 1 # ?????????????? miss for codecov install
 else
