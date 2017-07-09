bf639b945df9f02cb7acd90b470146aa14ef420b
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 11:19:20 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [[ $1 == '2.5' ]]; then
-  echo '*** Python - Updating Virtualenv...'
-  easy_install --user virtualenv==1.9.1 || exit 1
-fi
-
 echo '*** Python - Virtualenv setup in progress...'
 virtualenv -p "python$1" --setuptools "~/virtualenv/python$1" || exit 1
 source "~/virtualenv/python$1/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$1"
+
+if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
+  echo '*** Python - Updating Virtualenv...'
+  #easy_install virtualenv==1.9.1 || exit 1
+  pip install virtualenv==1.9.1 || exit 1
+fi
 
 virtualenv --version
 
