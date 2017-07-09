1ba305974ba0ac8c833f1e9584b53f6ad0263c69
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 14:45:59 +0200

    Update setup_dependencies.sh

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -2,6 +2,14 @@
 
 echo '*** Installing dependencies...'
 
+if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
+  pip install unittest2==0.8 || exit 1
+elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+  pip install unittest2==0.5.1 || exit 1
+else
+  pip install unittest2 || exit 1
+fi
+
 if [[ $TRAVIS_PYTHON_VERSION == '3.2' || $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   pip install coverage==4.0a5 || exit 1
   pip install requests==2.10.0 || exit 1
@@ -12,12 +20,7 @@ else
   :  # Coverage and requests are automatically installed by codecov.
 fi
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
-  pip install unittest2==0.8 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  pip install unittest2==0.5.1 || exit 1
-else
-  pip install unittest2 || exit 1
+if [[ $TRAVIS_PYTHON_VERSION != '2.5' && $TRAVIS_PYTHON_VERSION != '2.4' ]]; then
   pip install codecov || exit 1
 fi
 
