49654b0498fbe40acc1debd798fe861bf659dab0
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 13:45:52 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-echo '--------------------'
-easy_install-2.5 virtualenv==1.6.4
-echo '--------------------'
-pip-2.5 install virtualenv==1.6.4
-echo '--------------------'
-pip2.5 install virtualenv==1.6.4
-echo '--------------------'
-
 echo '*** Python - Virtualenv setup in progress...'
 virtualenv -p "python$1" --setuptools "~/virtualenv/python$1" || exit 1
 source "~/virtualenv/python$1/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$1"
 
-virtualenv --version
-
-exit 0
-
 echo '*** Python - Updating Virtualenv...'
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   easy_install virtualenv==13.1.2 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
-  #easy_install virtualenv==1.9.1 || exit 1
-  easy_install virtualenv==1.6.4 || exit 1
+  easy_install virtualenv==1.9.1 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   easy_install virtualenv==1.7.2 || exit 1
 else
   easy_install -U virtualenv || exit 1
 fi
 
+virtualenv --version
+
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   echo '*** Python - Updating Setuptools...'
-  pip install -U setuptools || exit 1
+  pip install setuptools==18.4 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   echo '*** Python - Updating Setuptools...'
   pip install setuptools==1.4.2 || exit 1
