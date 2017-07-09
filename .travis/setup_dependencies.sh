a9b1da222513809d62205ad036f97e29e54d9d94
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 14:36:26 +0200

    Update setup_dependencies.sh

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -2,7 +2,7 @@
 
 echo '*** Installing dependencies...'
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.2' ]]; then
+if [[ $TRAVIS_PYTHON_VERSION == '3.2' || $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   pip install coverage==4.0a5 || exit 1
   pip install requests==2.10.0 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.4' || $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
