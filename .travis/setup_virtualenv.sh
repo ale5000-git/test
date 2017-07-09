75a748ee7692230893559169e2272fc4f7019fb1
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 08:46:23 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -6,6 +6,8 @@ rm -f "~/virtualenv/python$1/bin/python" || exit 1
 virtualenv -p "/usr/bin/python$1" "~/virtualenv/python$1" || exit 1
 source "~/virtualenv/python$1/bin/activate" || exit 1
 
+virtualenv --version
+
 export TRAVIS_PYTHON_VERSION="$1"
 
 if [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
