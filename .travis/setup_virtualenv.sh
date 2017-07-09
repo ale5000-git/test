e698242bad672346f81acddd7abf1cdb5f9b569c
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 14:12:23 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -23,7 +23,7 @@ if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   echo '*** Python - Updating Setuptools...'
   easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
   rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
-  pip install setuptools==18.7 || exit 1
+  pip install setuptools==19.4.1 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   echo '*** Python - Updating Setuptools...'
   pip install setuptools==1.4.2 || exit 1
