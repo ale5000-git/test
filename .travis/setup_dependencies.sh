70475a289c6b2a421d9cf73a2c2e28acc9638d49
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 11:00:47 +0200

    Update setup_dependencies.sh

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -12,7 +12,9 @@ else
   :  # Coverage and requests are automatically installed by codecov.
 fi
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.1' || $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
+  pip install unittest2==0.6.0 || exit 1
+elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   pip install unittest2==0.5.1 || exit 1
 else
   pip install unittest2 || exit 1
