7de92ac29bada375ee1caf662036b46dab8f4610
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-10 15:15:31 +0200

    Update setup_dependencies.sh

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -3,25 +3,25 @@
 echo '*** Installing dependencies...'
 
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
-  pip install unittest2==0.8 || exit 1
+  pip install --disable-pip-version-check unittest2==0.8 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  pip install unittest2==0.5.1 || exit 1
+  pip install --disable-pip-version-check unittest2==0.5.1 || exit 1
 else
-  pip install unittest2 || exit 1
+  pip install --disable-pip-version-check unittest2 || exit 1
 fi
 
 if [[ $TRAVIS_PYTHON_VERSION == '3.2' || $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
-  pip install coverage==4.0a5 || exit 1
-  pip install requests==2.10.0 || exit 1
+  pip install --disable-pip-version-check coverage==4.0a5 || exit 1
+  pip install --disable-pip-version-check requests==2.10.0 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  pip install coverage==3.7.1 || exit 1
-  #pip install requests==2.7.9 || exit 1 # ?????????????? miss for codecov install
+  pip install --disable-pip-version-check coverage==3.7.1 || exit 1
+  #pip install --disable-pip-version-check requests==2.7.9 || exit 1 # ?????????????? miss for codecov install
 else
   :  # Coverage and requests are automatically installed by codecov.
 fi
 
 if [[ $TRAVIS_PYTHON_VERSION != '2.5' && $TRAVIS_PYTHON_VERSION != '2.4' ]]; then
-  pip install codecov || exit 1
+  pip install --disable-pip-version-check codecov || exit 1
 fi
 
 echo '*** Done.'
