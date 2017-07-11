d69939062a051e7211ad144df03f5324107827b0
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-11 20:29:52 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-if [[ -n "$1" ]]; then
-    echo 'NOT Empty'
-    VER="$1"
-else
-    echo 'Empty'
-    VER="$Python"
-fi
-echo $VER
+if [[ -n "$1" ]]; then VER="$1"; else VER="$Python"; fi
 
 echo '*** Python - Virtualenv setup in progress...'
-virtualenv -p "python$1" --setuptools "~/virtualenv/python$1" || exit 1
-source "~/virtualenv/python$1/bin/activate" || exit 1
+virtualenv -p "python$VER" --setuptools "~/virtualenv/python$VER" || exit 1
+source "~/virtualenv/python$VER/bin/activate" || exit 1
 
-export TRAVIS_PYTHON_VERSION="$1"
+export TRAVIS_PYTHON_VERSION="$VER"
 
 echo '*** Python - Updating Virtualenv...'
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
@@ -31,7 +24,7 @@ virtualenv --version
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   echo '*** Python - Updating Setuptools...'
   easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
-  rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
+  rm -rf "~/virtualenv/python$VER/lib/python$VER/site-packages/distribute-"* || exit 1
   pip install setuptools==19.4 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   echo '*** Python - Updating Setuptools...'
