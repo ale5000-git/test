f7ba15b9d75ca8821b5a68d2179814661f2c5fca
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-12 01:00:31 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -27,7 +27,8 @@ if [[ $VER == '2.4' ]]; then
   exit 0
   virtualenv -p "python$VER" "~/virtualenv/python$VER" || exit 1
 elif [[ $VER == '2.3' ]]; then
-  cp -pf "test/.travis/lib/subprocess.py" "~/.local/lib/python$VER/site-packages/" || exit 1
+  ls "$TRAVIS_BUILD_DIR"
+  cp -pf "test/.travis/lib/subprocess.py" "~/.local/lib/python$VER/site-packages/" #|| exit 1
   "python$VER" virtualenv.py -p "python$VER" --setuptools "~/virtualenv/python$VER" || exit 1
 else
   exit 0
