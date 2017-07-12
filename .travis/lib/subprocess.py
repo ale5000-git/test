fb7e1af833ac1ea85374f49a5f19cace1ed53598
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-12 21:12:44 +0200

    Update subprocess.py

--- .travis/lib/subprocess.py
+++ .travis/lib/subprocess.py
@@ -4,37 +4,38 @@
 import sys, os
 
 class CalledProcessError(Exception):
-	"""Raised when a process run by check_call() or check_output()
-	returns a non-zero exit status."""
+    """Raised when a process run by check_call() or check_output()
+    returns a non-zero exit status."""
 
-	def __init__(self, returncode, cmd, output=None, stderr=None):
-		self.returncode = returncode
-		self.cmd = cmd
-		self.output = output
-		self.stdout = output
-		self.stderr = stderr
+    def __init__(self, returncode, cmd, output=None, stderr=None):
+        self.returncode = returncode
+        self.cmd = cmd
+        self.output = output
+        self.stdout = output
+        self.stderr = stderr
 
 def Popen(*args, **kwargs):
-	#process = os.popen(*args)
-	#return process.read()
+    pass
+    #process = os.popen(*args)
+    #return process.read()
 
 def check(*args, **kwargs):
-	if "stdout" in kwargs:
-		raise ValueError("stdout argument not allowed, "
-						 "it will be overridden.")
-	process = os.popen(*args)
-	print process.read()
+    if "stdout" in kwargs:
+        raise ValueError("stdout argument not allowed, "
+                         "it will be overridden.")
+    process = os.popen(*args)
+    print process.read()
 
 def check_call(*args, **kwargs):
-	if "stdout" in kwargs:
-		raise ValueError("stdout argument not allowed, "
-						 "it will be overridden.")
-	process = os.popen(*args)
-	print process.read()
+    if "stdout" in kwargs:
+        raise ValueError("stdout argument not allowed, "
+                         "it will be overridden.")
+    process = os.popen(*args)
+    print process.read()
 
 def check_output(*args, **kwargs):
-	if "stdout" in kwargs:
-		raise ValueError("stdout argument not allowed, "
-						 "it will be overridden.")
-	process = os.popen(*args)
-	return process.read()
+    if "stdout" in kwargs:
+        raise ValueError("stdout argument not allowed, "
+                         "it will be overridden.")
+    process = os.popen(*args)
+    return process.read()
