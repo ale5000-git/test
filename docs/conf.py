483c6697507c093843e9fe7458a729d12c71932c
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-20 18:48:03 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,17 @@ author = 'ale5000'
 # The master toctree document.
 master_doc = 'index'
 
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'pycompatlayer')))
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
+    'sphinx.ext.doctest'
 ]
 
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'pycompatlayer')))
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
