20ee6e0464d9ca5625ee96a372fea76c1561e147
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-03 17:13:55 +0200

    Update pycompatlayer.py

--- pycompatlayer/pycompatlayer.py
+++ pycompatlayer/pycompatlayer.py
@@ -159,7 +159,7 @@ def fix_subprocess(override_debug=False, override_exception=False):
     """Activate the subprocess compatibility."""
     import subprocess
 
-    class CalledProcessError(Exception):
+    class CalledProcessError(Exception, object):
         """Raised when a process run by check_call() or check_output()
         returns a non-zero exit status."""
 
