dd967fb51abae79541da3d7bc9fba454d4a499d5
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-08 15:14:37 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -30,7 +30,7 @@ cd ..
 echo '*** Python - Virtualenv setup in progress...'
 
 if [[ $VER == '3.1' ]]; then
-  "virtualenv-$VER" -p "python$VER" --setuptools "$HOME/virtualenv/python$VER" || exit 1
+  "virtualenv-$VER" -p "python$VER" --no-pip "$HOME/virtualenv/python$VER" || exit 1
 elif [[ $VER == '_2.4' ]]; then
   virtualenv -p "python$VER" "$HOME/virtualenv/python$VER" || exit 1
 elif [[ $VER == '_2.3' ]]; then
