8c82f63f9bb8b35dc658731dda54b3a5607423f9
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-09 01:59:04 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -2,7 +2,6 @@
 
 if [[ -n "$1" ]]; then VER="$1"; else VER="$Python"; fi
 
-echo '*** Python - Installing Virtualenv...'
 if [[ $VER == '3.1' ]]; then
   VENV_VER='13.1.2'
 elif [[ $VER == '2.5' ]]; then
@@ -12,59 +11,38 @@ elif [[ $VER == '2.4' ]]; then
 elif [[ $VER == '2.3' ]]; then
   VENV_VER='1.3.1'
   ############### 1.3.2
-else
-  #easy_install -U virtualenv || exit 1
-  VENV_VER='x.x' #################################
 fi
 
-if [[ $VER == '2.3' ]]; then
-  easy_install setuptools==1.4.2 || exit 1 #################
+if [[ -n "$VENV_VER" ]]; then
+  echo '*** Python - Installing Virtualenv...'
+  wget -q "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-${VENV_VER}.tar.gz" || exit 1
+  tar -xz -f "virtualenv-${VENV_VER}.tar.gz" || exit 1
+  cd "virtualenv-${VENV_VER}/" || exit 1
+  "python$VER" setup.py install --prefix="$HOME/.local" || exit 1
+  cd .. || exit 1
 fi
-
-wget -q "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-${VENV_VER}.tar.gz" || exit 1
-tar -xz -f "virtualenv-${VENV_VER}.tar.gz" || exit 1
-cd "virtualenv-${VENV_VER}/"
-"python$VER" setup.py install --prefix="$HOME/.local" || exit 1
-cd ..
 
 echo '*** Python - Virtualenv setup in progress...'
-
 if [[ $VER == '3.1' ]]; then
   "virtualenv-$VER" -p "python$VER" --no-setuptools "$HOME/virtualenv/python$VER" || exit 1
-elif [[ $VER == '_2.4' ]]; then
-  virtualenv -p "python$VER" "$HOME/virtualenv/python$VER" || exit 1
-elif [[ $VER == '_2.3' ]]; then
-  cp -pf "$TRAVIS_BUILD_DIR/.travis/lib/subprocess.py" "$HOME/.local/lib/python$VER/site-packages" || exit 1
-  pip install setuptools==1.4.2 || exit 1 #################
-  "python$VER" "$HOME/.local/lib/python$VER/site-packages/virtualenv.pyc" -p "python$VER" "$HOME/virtualenv/python$VER" #|| exit 1
+elif [[ $VER == '2.3' ]]; then
+  "python$VER" "$HOME/.local/lib/python$VER/site-packages/virtualenv" -p "python$VER" "$HOME/virtualenv/python$VER" #|| exit 1
 else
   "virtualenv-$VER" -p "python$VER" "$HOME/virtualenv/python$VER" || exit 1
 fi
-source "$HOME/virtualenv/python$VER/bin/activate" #|| exit 1
+source "$HOME/virtualenv/python$VER/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$VER"
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
-  echo '*** Python - Updating Setuptools...'
-  easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
-  rm -rf "$HOME/virtualenv/python$VER/lib/python$VER/site-packages/distribute-"* || exit 1
-  pip install setuptools==19.4 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '_2.5' ]]; then
-  echo '*** Python - Updating Setuptools...'
-  #easy_install setuptools==1.4.2 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '_2.4' ]]; then
-  echo '*** Python - Updating Setuptools...'
-  #pip install setuptools==1.4.2 || exit 1
-fi
+#if [[ $VER == '2.3' ]]; then
+  #easy_install setuptools==1.4.2 || exit 1 #################
+  #cp -pf "$TRAVIS_BUILD_DIR/.travis/lib/subprocess.py" "$HOME/.local/lib/python$VER/site-packages" || exit 1
+  #
+#fi
 
 if [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
-  echo '*** Python - Updating Pip...'
-  #easy_install pip==1.2.1 || exit 1  # Workaround for missing SSL
-  #easy_install ssl || exit 1  # Needed by pip 1.3.1
-  #pip install pip==1.3.1 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  echo '*** Python - Updating Pip...'
-  pip install pip==1.1 || exit 1
+  echo '*** Python - Downgrading Pip (Workaround for missing SSL in Python 2.5)...'
+  easy_install pip==1.2.1 || exit 1
 fi
 
 echo '*** Done.'
