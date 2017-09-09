84c748154c2b1f02949b6b0006c70be1272c1c35
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-09 03:41:10 +0200

    Update setup_virtualenv.sh

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -30,7 +30,7 @@ fi
 
 echo '*** Python - Virtualenv setup in progress...'
 if [[ $VER == '3.1' ]]; then
-  "virtualenv-$VER" -p "python$VER" --no-setuptools "$HOME/virtualenv/python$VER" || exit 1
+  "virtualenv-$VER" -p "python$VER" --no-setuptools --no-pip "$HOME/virtualenv/python$VER" || exit 1
 elif [[ $VER == '2.3' ]]; then
   "python$VER" "$HOME/.local/lib/python$VER/site-packages/virtualenv.py" -p "python$VER" "$HOME/virtualenv/python$VER" ###|| exit 1
    echo '...'
@@ -56,7 +56,7 @@ echo '------------------'
 
 if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   echo '*** Python - Downgrading Pip...'
-  easy_install pip==1.5.6 || exit 1
+  #easy_install pip==1.5.6 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' ]]; then
   echo '*** Python - Downgrading Pip (Workaround for missing SSL in Python 2.5)...'
   easy_install pip==1.2.1 || exit 1
